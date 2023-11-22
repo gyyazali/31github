b74507865ec9c8760546696a09ab3ac294195a69
unknown <giyaz>
2023-11-22 18:03:14 +0300

    add pizza cards and active nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import "../../src/App.css"
+import React from "react"
 import shopCar from "../assets/shop-car.png"
 import pizzaIcon from "../assets/pizzaIcon.png"
 import { Pizza_card } from "./pizza_card/pizza_card";
 
 function App() {
-  return (
-    <div className="App">
+  const [activeCategory, setActiveCategory] = React.useState(0)
+
+  const onClickCategory = (i) => {
+    setActiveCategory(i)
+  }
+ return (
+     <div className="App">
       <div className="content">
         <div className="header">
           <div className="pizzaIcon">
@@ -26,12 +32,12 @@ function App() {
         </div>
         <nav className="nav">
           <div className="category">
-            <p className="category_name_active category_name">Все</p>
-            <p className="category_name">Мясные</p>
-            <p className="category_name">Вегетарианская</p>
-            <p className="category_name">Гриль</p>
-            <p className="category_name">Острые</p>
-            <p className="category_name">Закрытые</p>
+            <p onClick={() => onClickCategory(0)} className={`${activeCategory === 0 ? "category_name_active" : ""} category_name`}>Все</p>
+            <p onClick={() => onClickCategory(1)} className={`${activeCategory === 1 ? "category_name_active" : ""} category_name`}>Мясные</p>
+            <p onClick={() => onClickCategory(2)} className={`${activeCategory === 2 ? "category_name_active" : ""} category_name`}>Вегетарианская</p>
+            <p onClick={() => onClickCategory(3)} className={`${activeCategory === 3 ? "category_name_active" : ""} category_name`}>Гриль</p>
+            <p onClick={() => onClickCategory(4)} className={`${activeCategory === 4 ? "category_name_active" : ""} category_name`}>Острые</p>
+            <p onClick={() => onClickCategory(5)} className={`${activeCategory === 5 ? "category_name_active" : ""} category_name`}>Закрытые</p>
           </div>
           <div className="sort">
             Сортировка по:
@@ -43,6 +49,11 @@ function App() {
         <div className="pizzas">
           <p className="pizzas_title">Все пиццы</p>
           <div className="pizza_cards">
+            <Pizza_card />
+            <Pizza_card />
+            <Pizza_card />
+            <Pizza_card />
+            <Pizza_card />
             <Pizza_card />
           </div>
         </div>
