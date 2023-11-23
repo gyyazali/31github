a98b15f5be02bf580a21783d2c4b0de81d7dd5fb
gyyazali <giyaz>
2023-11-23 16:17:19 +0300

    active popup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,30 @@
-import "../../src/App.css"
-import React from "react"
-import shopCar from "../assets/shop-car.png"
-import pizzaIcon from "../assets/pizzaIcon.png"
-import { Pizza_card } from "./pizza_card/pizza_card";
-import pizzas from "../assets/pizzas.json";
+import '../../src/App.css';
+import React from 'react';
+import shopCar from '../assets/shop-car.png';
+import pizzaIcon from '../assets/pizzaIcon.png';
+import { Pizza_card } from './pizza_card/pizza_card';
+import pizzas from '../assets/pizzas.json';
 
 function App() {
-  const [activeCategory, setActiveCategory] = React.useState(0)
-  console.log(pizzas);
-  const categories = ["Все", "Мясные", "Вегетарианская", "Гриль", "Острые", "Закрытые"]
+  const [activeCategory, setActiveCategory] = React.useState(0);
+  const [selected, setSelected] = React.useState(0);
+  const [popupActive, setPopupActive] = React.useState(false);
+
+  const listSort = ['популярности', 'цену', 'алфавиту'];
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
   const onClickCategory = (i) => {
-    setActiveCategory(i)
-  }
- return (
-     <div className="App">
+    setActiveCategory(i);
+  };
+
+  const popupSelected = (i) => {
+    setSelected(i);
+    setPopupActive(false);
+  };
+
+  const popupName = listSort[selected];
+  return (
+    <div className="App">
       <div className="content">
         <div className="header">
           <div className="pizzaIcon">
@@ -27,31 +38,48 @@ function App() {
             <span className="price_num">199 c</span>
             <div className="price_line"></div>
             <span className="price_count">
-              <img src={shopCar} alt="" />
-              3
+              <img src={shopCar} alt="" />3
             </span>
           </div>
         </div>
         <nav className="nav">
           <div className="category">
-            {
-              categories.map((value, i) => (<p onClick={() => onClickCategory(i)} className={`${activeCategory === i ? "category_name_active" : ""} category_name`}>{value}</p>))
-            }
+            {categories.map((value, i) => (
+              <p
+                onClick={() => onClickCategory(i)}
+                className={`${activeCategory === i ? 'category_name_active' : ''} category_name`}
+              >
+                {value}
+              </p>
+            ))}
           </div>
           <div className="sort">
             Сортировка по:
-            <p className="sort_name">
-              популярности
+            <p
+              onClick={() => {
+                setPopupActive(!popupActive);
+              }}
+              className="sort_name"
+            >
+              {popupName}
             </p>
+            {popupActive && (
+              <div className="popup">
+                {listSort.map((name, i) => (
+                  <p onClick={() => popupSelected(i)} key={i}>
+                    {name}
+                  </p>
+                ))}
+              </div>
+            )}
           </div>
         </nav>
         <div className="pizzas">
           <p className="pizzas_title">Все пиццы</p>
           <div className="pizza_cards">
-            {
-              pizzas.map((obj) => (<Pizza_card {...obj}/>))
-            }
-            
+            {pizzas.map((obj) => (
+              <Pizza_card {...obj} />
+            ))}
           </div>
         </div>
       </div>
--- src/components/pizza_card/pizza_card.jsx
+++ src/components/pizza_card/pizza_card.jsx
@@ -1,34 +1,42 @@
-import React from "react"
-import css from "./pizza_card.module.css"
+import React from 'react';
+import css from './pizza_card.module.css';
 
-export const Pizza_card = ({title, price, imageUrl, sizes, types}) => {
-    const [activeType, setActiveType] = React.useState(0)
-    const [activeSize, setActiveSize] = React.useState(0)
-    const typeName = ["тонкое","традиционное"] 
-    
-    return (
-        <div className={css.card}>
-            <img src={imageUrl} alt="" className={css.card_img} />
-            <p className={css.card_name}>{title}</p>
-            <div className={css.card_kind}>
-                {
-                    types.map((typeIndex) => (<p onClick={() => setActiveType(typeIndex)} className={activeType === typeIndex ? css.card_kind_name_active : ""}>{typeName[typeIndex]}</p>))
-                }
-            </div>
-            <div className={css.card_size}>
-                {
-                    sizes.map((size, i) => (<p onClick={() => setActiveSize(i)} className={activeSize === i ? css.card_size_sm_active : ""}>{size} см.</p>))
-                }
-            </div>
-            <div className={css.card_price}>
-                <div className={css.price_num}>
-                    от {price} с
-                </div>
-                <div className={css.price_button}>
-                   + Добавить
-                    <span>0</span>
-                </div>
-            </div>
+export const Pizza_card = ({ title, price, imageUrl, sizes, types }) => {
+  const [activeType, setActiveType] = React.useState(0);
+  const [activeSize, setActiveSize] = React.useState(0);
+  const typeName = ['тонкое', 'традиционное'];
+
+  return (
+    <div className={css.card}>
+      <img src={imageUrl} alt="" className={css.card_img} />
+      <p className={css.card_name}>{title}</p>
+      <div className={css.card_kind}>
+        {types.map((typeIndex) => (
+          <p
+            onClick={() => setActiveType(typeIndex)}
+            className={activeType === typeIndex ? css.card_kind_name_active : ''}
+          >
+            {typeName[typeIndex]}
+          </p>
+        ))}
+      </div>
+      <div className={css.card_size}>
+        {sizes.map((size, i) => (
+          <p
+            onClick={() => setActiveSize(i)}
+            className={activeSize === i ? css.card_size_sm_active : ''}
+          >
+            {size} см.
+          </p>
+        ))}
+      </div>
+      <div className={css.card_price}>
+        <div className={css.price_num}>от {price} с</div>
+        <div className={css.price_button}>
+          + Добавить
+          <span>0</span>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
