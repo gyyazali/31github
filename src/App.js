d96f0c7aed09015f13fee9b17c980253b3160470
gyyazali <giyaz>
2023-12-13 23:33:30 +0600

    fixed bug in get items pizza

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import React, { useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import { setCategoryId, setFilters } from './redux/slices/filterSlice';
+import { setFilters } from './redux/slices/filterSlice';
 import { useNavigate } from 'react-router-dom';
 import qs from 'qs';
-import { list } from './components/Sort/Sort';
+import { sortList } from './components/Sort/Sort';
 import Main from './pages/Main/Main';
 import NotFound from './pages/NotFound/NotFound';
 import Basket from './pages/Basket/Basket';
@@ -18,31 +18,14 @@ function App() {
   const { categoryId, sort, currentPage } = useSelector((state) => state.filter);
   const [searchValue, setSearchValue] = React.useState('');
   const isMounted = useRef(false);
-  const onClickCategory = (id) => {
-    dispatch(setCategoryId(id));
-  };
 
-  //Если изменили параметры и был первый рендер
-  React.useEffect(() => {
-    if (isMounted.current) {
-      const queryString = qs.stringify({
-        sortProperty: sort.sortProperty,
-        categoryId,
-        currentPage,
-      });
-      navigate(`?${queryString}`);
-    }
-    isMounted.current = true;
-  }, [categoryId, sort.sortProperty, currentPage]);
-
-  // Если был первый рендер, то проверяем URL - параметры и сохраняем в redux
-
-  const getPizzas = async () => {
+  const getPizzas = () => {
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const order = sort.sortProperty.includes('-') ? 'asc' : 'desc';
     const sortBy = sort.sortProperty.replace('-', '');
     const search = searchValue ? `&search=${searchValue}` : '';
 
+    console.log('efesfe)');
     dispatch(
       fetchPizzas({
         category,
@@ -56,16 +39,33 @@ function App() {
     window.scrollTo(0, 0);
   };
 
+  //Если изменили параметры и был первый рендер
   React.useEffect(() => {
-    if (window.location.search) {
-      getPizzas();
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sort.sortProperty,
+        categoryId,
+        currentPage,
+      });
+      navigate(`?${queryString}`);
     }
+
+    if (!window.location.search) {
+      fetchPizzas();
+    }
+
+    isMounted.current = true;
+  }, [categoryId, sort.sortProperty, currentPage]);
+
+  // Если был первый рендер, то проверяем URL - параметры и сохраняем в redux
+  React.useEffect(() => {
+      getPizzas();
   }, [categoryId, sort.sortProperty, searchValue, currentPage]);
-  
+
   React.useEffect(() => {
     if (window.location.search) {
       const params = qs.parse(window.location.search.substring(1));
-      const sort = list.find((obj) => obj.sortProperty === params.sortProperty);
+      const sort = sortList.find((obj) => obj.sortProperty === params.sortProperty);
       dispatch(
         setFilters({
           ...params,
@@ -74,15 +74,13 @@ function App() {
       );
     }
   }, []);
+
   return (
     <div className="App">
       <div className="content">
         <Header setSearchValue={setSearchValue} />
         <Routes>
-          <Route
-            path="/"
-            element={<Main onClickCategory={onClickCategory} />}
-          />
+          <Route path="/" element={<Main />} />
           <Route path="/Basket" element={<Basket />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
