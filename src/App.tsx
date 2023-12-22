65756917ba57d728bb8a9ae0762640856373b139
gyyazali <giyaz>
2023-12-22 20:47:33 +0600

    use React lazy for optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import './App.css';
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
-import NotFound from './pages/NotFound/NotFound';
-import Basket from './pages/Basket/Basket';
-import FullPizza from './pages/FullPizza/FullPizza';
 import MainLayout from './layouts/MainLayout';
+
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound"*/ './pages/NotFound/NotFound'));
+const Basket = React.lazy(() => import(/* webpackChunkName: "Basket"*/ './pages/Basket/Basket'));
+const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza"*/ './pages/FullPizza/FullPizza'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Main />} />
-        <Route path="Basket" element={<Basket />} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="Basket"
+          element={
+            <Suspense fallback={<div>Идет загрузка корзины...</div>}>
+              <Basket />
+            </Suspense>
+          }
+        />
+        <Route
+          path="pizza/:id"
+          element={
+            <Suspense fallback={<div>Идет загрузка...</div>}>
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Идет загрузка...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
